0164142c1db1b7bfcf68d951e9ae2feca9c76fab
Raushan <raushan.kumar.cer20@itbhu.ac.in>
2024-03-24 15:35:18 +0530

    ui complete, payments needs to be integrated

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+import FeaturedCourses from "./components/FeaturedCourses";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <HeroSection/>
+    <FeaturedCourses/>
+    <Footer/>
+  </main>
+  );
+}
