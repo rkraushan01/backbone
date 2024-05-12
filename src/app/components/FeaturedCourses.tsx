ca762d1458832f3e60ba40dc2bcabe37d74fdd5c
Raushan <raushan.kumar.cer20@itbhu.ac.in>
2024-05-12 15:57:27 +0530

    button improved

--- src/app/components/FeaturedCourses.tsx
+++ src/app/components/FeaturedCourses.tsx
@@ -44,7 +44,7 @@ function FeaturedCourses() {
                   </p>
                   <Link className="text-white" href={`/courses/${course.slug}`}>
                     <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl"
                     >
                     Register
                     </button>
